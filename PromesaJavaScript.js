9d203c993b2476726fe83725b1a6f327f93b3ae0
alexander lopez <alexander lopez>
2017-05-25 22:44:07 -0500

    actualizar

--- PromesaJavaScript.js
+++ PromesaJavaScript.js
@@ -1,4 +1,4 @@
-var prom = new Promise((resolve,rejec)=>{
+var prom = new Promise((resolve,reject)=>{
     setTimeout(()=> {
         resolve(5);
     },5000);
@@ -6,3 +6,44 @@ var prom = new Promise((resolve,rejec)=>{
 prom.then((numero)=>{
     console.log(numero);
 })
+.catch((err)=>{
+    console.log("hay un error:${err}");
+})
+
+
+// asincrona
+
+var prom = new Promise(function(resolve,reject){
+const numero = 5;
+if (numero == 15)
+resolve("el numero es !!");
+else
+reject(Error("hay un error"));
+
+});
+promise.then(function(respuesta){
+console.log(respuesta);
+}, function(error){
+    console.log(error);
+
+})
+
+// setTimeout
+
+var prom = new  Promise(function(resolve,reject){
+    setTimeout(function(){
+        const numero = 5;
+        if (numero == 15)
+        resolve("llego promesa!!!");
+else
+reject(Error("hay un error"));
+
+
+    
+prom.then(function(respuesta){
+    console.log(respuesta);
+}, function(error){
+    console.log(error);
+});
+    }, 3000);
+});
